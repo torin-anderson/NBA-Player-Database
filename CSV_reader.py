f2fb8d390cfef6bf3e44cadf29353db295e93fe6
torin-anderson <atorin16@gmail.com>
2023-12-12 13:44:44 -0500

    final draft

--- CSV_reader.py
+++ CSV_reader.py
@@ -1,36 +1,47 @@
-#import os
-#import pandas as pd
-
-#master_df = pd.DataFrame()
-
-#for file in os.listdir(os.getcwd()):
-    #if file.endswith('.csv'):
-        #master_df = master_df.append(pd.read_csv(file))
-
-#master_df.to_csv('Master FILE NBA.CSV', index=False)
-
-
-
-#import pandas as pd
-import glob
-import os
-
-#path = r'/Users/gabesfolder/Desktop/NBA CSV' # use your path
-#all_files = glob.glob(os.path.join(path , "/*.csv"))
-
-#li = []
-
-#for filename in all_files:
-    #df = pd.read_csv(filename, index_col=None, header=0)
-    #li.append(df)
-
-#frame = pd.concat(li, axis=0, ignore_index=True)
-
 import os
 import pandas as pd
 
-files = []
-for file in os.listdir(os.getcwd()):
-    if file.endswith('.csv'):
-        df = pd.read_csv(files)
-        files.append(df)
+ps = []
+rg = []
+
+def read_data():
+    '''
+    This function reads each of the csv files and adds them to a list depending on whether it's a csv for post season or regular season stats.
+    '''
+
+    for file in os.listdir(os.getcwd()): #iterates through each of the csv files
+        if file.endswith('.csv'): 
+            df = pd.read_csv(file)
+            df = df.set_index('Player')
+            if 'ps' in file: #appends post season list if the title of the csv tells it's for postseason stats
+                ps.append(df)   
+            else: #appends regular season list if the csv is not for postseason
+                rg.append(df)
+
+def find_player(player, stat, PS=0, RG=0):
+    '''
+    This function searches for the player in each of the csv files and then, if the player is found, returns the 
+    selected stat into two lists with one for the regular season findings and one for the post season findings
+
+    Has four parameters: player, stat, PS, RG.
+        player is searched for through each of the csv files and if found allows data to be taken from that csv file.
+        stat is what data is taken from the player if the player is found in the csv file.
+        PS and RG are set at 0 and determine how many times the csv is searched through.
+    '''
+
+    result_ps = []
+    result_rg = []
+
+    if PS:
+        for frame in ps:
+            if player in frame.index: #if player is found in post season csv, then the stat will be appended to result_ps
+                result_ps.append(frame.loc[player,stat])
+            elif len(result_ps) >= 1 and player not in frame.index: #if player is in the league, but missed post season, then a 0 acts as place holder
+                 result_ps.append(0)
+    if RG: 
+        for frame in rg:
+            if player in frame.index: #if player is found in regular season csv, then the stat will be appended to result_rg
+                result_rg.append(frame.loc[player,stat])
+            elif len(result_rg) >= 1 and player not in frame.index: #if player is in the league, but missed regular season, then a 0 acts as place holder
+                 result_rg.append(0)
+    return result_ps, result_rg
--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 from tkinter import *
 import tkinter as tk
 import matplotlib.pyplot as plt
-import CSV_reader as stat_tracker
+import CSV_reader as stats
 
-years = ["2013-2014", "2014-2015", "2015-2016", "2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021", "2021-2022", "2022-2023"]
+years = ['2013-2014','2014-2015','2015-2016',
+         '2016-2017','2017-2018','2018-2019',
+         '2019-2020','2020-2021','2021-2022','2022-2023'] #establishes all possible years that can be viewed
+stats.read_data() #reads all of the csv files
+
 
 #Establishes the first window where it asks the user for a player and takes in the player's name
 master = Tk()
@@ -12,11 +16,10 @@ master.title("User Input for Player")
 master.rowconfigure(0,minsize=300, weight=1)
 master.columnconfigure(0,minsize=300,weight=1)
 
-#The first window where it asks the user for a player and takes in the player's name
 frame = Frame(master, relief=RAISED)
 frame.grid(row=0, column=0, sticky="ns")
 
-#the text box on the first window
+#the text box on the top of the first window that asks which nba player the user wants to view
 text_box = Label(frame,
                 width=60,
                 height=3,
@@ -36,8 +39,19 @@ btn_continue = Button(frame,
                 command=lambda : stat_window(input.get().title()))
 btn_continue.place(x=180, y=165)
 
-#Function creates the second window which determines which stat whill be displayed on the graph
+
 def stat_window(name):
+    '''
+    This function creates the second window that has 10 
+    buttons to select different stats to view on the graph in the next window.
+
+    Has one parameter: name. 
+        Name  is taken from the previous window through the entry box and the button brings it to this function.
+
+    Each button in this window brings the user to a graph that compares the player's career
+    and the stat for the button they clicked. 
+    '''
+
     window2 = Toplevel(master)
     window2.title("User Input for Stats")
 
@@ -47,13 +61,25 @@ def stat_window(name):
     frame2 = Frame(window2, relief=RAISED)
     frame2.grid(row=0, column=0, sticky="ns")
 
-    #Text Box
+    #the text box at the top of the second window that asks which stat would like to be viewed
     text_box = Label(frame2,
                     width=60,
                     height=3,
                     text="Which stat would you like to view?",
                     bg="LIGHTBLUE")
     text_box.grid(row=0,columnspan=2)
+
+    #Retreiving the numbers for each type of stat for the inputted player
+    ppg= stats.find_player(name,'PTS',1,1)
+    mpg = stats.find_player(name,'MP',1,1)
+    gp = stats.find_player(name,'G',1,1)
+    eFG = stats.find_player(name,'eFG%',1,1)
+    ft = stats.find_player(name,'FT%',1,1)
+    rb = stats.find_player(name,'TRB',1,1)
+    ast= stats.find_player(name,'AST',1,1)
+    stl = stats.find_player(name,'STL',1,1)
+    tov= stats.find_player(name,'TOV',1,1)
+    blk = stats.find_player(name,'BLK',1,1)
 
     #Making each of the buttons for the second window
     #Points per game button
@@ -62,8 +88,8 @@ def stat_window(name):
                     height=3,
                     text="Points Per Game",
                     command=lambda : line_graph("PPG", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                ppg[1], 
+                                                ppg[0], 
                                                 name))
     btn_ppg.grid(row=1, column=0)
 
@@ -73,8 +99,8 @@ def stat_window(name):
                     height=3,
                     text="Minutes Per Game",
                     command=lambda : line_graph("MPG", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                mpg[1], 
+                                                mpg[0], 
                                                 name))
     btn_mpg.grid(row=1, column=1)
 
@@ -84,8 +110,8 @@ def stat_window(name):
                     height=3,
                     text="Games Per Season",
                     command=lambda : line_graph("GP", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                gp[1], 
+                                                gp[0], 
                                                 name))
     btn_games.grid(row=2, column=0)
 
@@ -95,21 +121,21 @@ def stat_window(name):
                     height=3,
                     text="eFG% Per Season",
                     command=lambda : line_graph("eFG%", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                eFG[1], 
+                                                eFG[0], 
                                                 name))
     btn_efg.grid(row=2, column=1)
 
     #Free throws per season
-    btn_games = Button(frame2,
+    btn_ft = Button(frame2,
                     width=30,
                     height=3,
                     text="FT% Per Season",
                     command=lambda : line_graph("FT%", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                ft[1], 
+                                                ft[0], 
                                                 name))
-    btn_games.grid(row=3, column=0)
+    btn_ft.grid(row=3, column=0)
 
     #Rebounds per game
     btn_rebounds = Button(frame2,
@@ -117,8 +143,8 @@ def stat_window(name):
                     height=3,
                     text="Rebounds Per Game",
                     command=lambda : line_graph("TRB", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                rb[1], 
+                                                rb[0], 
                                                 name))
     btn_rebounds.grid(row=3, column=1)
 
@@ -128,8 +154,8 @@ def stat_window(name):
                     height=3,
                     text="Assists Per Game",
                     command=lambda : line_graph("AST", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                ast[1], 
+                                                ast[0], 
                                                 name))
     btn_assists.grid(row=4, column=0)
 
@@ -139,8 +165,8 @@ def stat_window(name):
                     height=3,
                     text="Steals Per Game",
                     command=lambda : line_graph("STL", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                stl[1], 
+                                                stl[0], 
                                                 name))
     btn_steals.grid(row=4, column=1)
 
@@ -149,9 +175,9 @@ def stat_window(name):
                     width=30,
                     height=3,
                     text="Turnovers Per Game",
-                    command=lambda : line_graph("TOV", 
-                                                [6, 20, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [5, 8, 10, 12, 6, 9, 7, 0, 5, 2], 
+                    command=lambda : line_graph("TOV",  
+                                                tov[1],
+                                                tov[0], 
                                                 name))
     btn_turnovers.grid(row=5, column=0)
 
@@ -161,27 +187,42 @@ def stat_window(name):
                     height=3,
                     text="Blocks Per Game",
                     command=lambda : line_graph("BLK", 
-                                                [0, 0, 15, 0, 5, 8, 10, 12, 6, 12], 
-                                                [0, 0, 10, 12, 6, 9, 7, 0, 5, 2], 
+                                                blk[1], 
+                                                blk[0], 
                                                 name))
     btn_blocks.grid(row=5, column=1)
 
 #Function that creates the graph
-def line_graph(stat_type, rg_stat, ps_stat, player):
+def line_graph(stat_type, rg_stat, ps_stat, name):
+    '''
+    This function creates the third window that displays a line graph that compares
+    each season that the player played and the stat the user chose to view, the seasons
+    acting as the x-axis and the stat as the y-axis.
+
+    Has four parameters: stat_type, rg_stat, ps_stat, and name. 
+        stat_type follows which stat the user selected to view and displays the name of that stat as the label for the y-axis.
+        rg_stat and ps_stat are the regular season and post_season numbers for the stat shown and are both plotted on the graph, 
+            rg_stat as blue and ps_stat as orange. If a season on the graph was missed by the player then the stat is shown as 0.
+        name is used as the title for the graph
+    '''
 
     fig, ax = plt.subplots()
-    ax.plot(years, rg_stat, label="Regular Season")
-    ax.plot(years, ps_stat, label="Playoffs") 
+    ax.plot(years[10 - len(rg_stat):], #the x-axis starting when the player started or if they played before 2013-2014 then starts 2013-2014 season 
+            rg_stat, 
+            label="Regular Season") #plots regular season 
+    ax.plot(years[10 - len(rg_stat):], #the x-axis starting when the player started or if they played before 2013-2014 then starts 2013-2014 season 
+            ps_stat, 
+            label="Playoffs") #plots post season stat,
     ax.legend()
 
     #titles
-    ax.set_title(player, loc='center', fontsize="xx-large")
+    ax.set_title(name, loc='center', fontsize="xx-large")
     ax.set_ylabel(stat_type)
     ax.set_xlabel("Seasons")
 
     #style changes for graph
     ax.grid(True) #creates grid
-    plt.xticks(fontsize=6)
+    plt.xticks(fontsize=6) #changes font size for x-axis ticks or seasons nubmers
 
     plt.show()
 
